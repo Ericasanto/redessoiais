3f79a4186745927b12d24dd70f35eabfbefe7908
Erica_Marco <126908056+Ericasanto@users.noreply.github.com>
2024-11-07 11:13:53 +0000

    7/11

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -3,7 +3,13 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const paragrafo= document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML=`você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectados} estão conectadas em alguma rede sociale passam em media ${dados.tempo_medio} horas conectadas`
+    console.log(paragrafo)
+
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
